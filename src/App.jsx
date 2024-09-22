4e35837481d57eb0dcd134c6a23144c349201a77
Birhan habtamu <berahaan@gmail.com>
2024-09-22 20:26:30 +0300

    install react plus tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-lg text-teal-300">
+      App
+      {/* we can use a control plus space for a suggestions  */}
+    </div>
+  );
+}
+
+export default App;
