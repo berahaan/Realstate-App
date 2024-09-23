93e86b059e35d53290f96130be4e4991bf92cbd8
Birhan habtamu <berahaan@gmail.com>
2024-09-23 23:43:38 +0300

    creating responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
 function App() {
   return (
     <Router>
+      <Header />
+      {/* this can help us to make this pages to  be appeared on every pages or components  */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/about" element={<Home />}></Route>
-        <Route path="/contact" element={<Home />}></Route>
+
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
       </Routes>
     </Router>
   );
