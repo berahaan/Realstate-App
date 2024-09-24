ce4972dc1ce81963df65acd84fa46e164a657be3
Birhan habtamu <berahaan@gmail.com>
2024-09-24 22:17:00 +0300

    configuring mongodb atlas

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is successfully connected be happy man ");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 const app = express();
 app.listen(2000, () => {
   console.log("the server is running in the port of 2000");
